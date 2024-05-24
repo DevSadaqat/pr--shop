eb14c87d3008b2d2e438e3813b6dc4478008541a
Sadaqat Ali <entrepreneurali@outlook.com>
2024-05-24 10:15:18 +1000

    feat: User Auth endpoint

---
+++ backend/controllers/userController.js
@@ -0,0 +1,92 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
+// import User from '../models/userModel';
+
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+// @desc    Register user
+// @route   POST /api/users/register
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send('register user');
+});
+
+// @desc    Logout user
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send('logout user');
+});
+
+// @desc    Get user Profile
+// @route   GET /api/users/profile
+// @access  Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('get user profile');
+});
+
+// @desc    Update User Profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send('update user profile');
+});
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send('get users');
+});
+
+// @desc    Get users by ID
+// @route   GET /api/users
+// @access  Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send('get user by ID');
+});
+
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send('delete users');
+});
+
+// @desc    Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send('update user');
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  getUserById,
+  deleteUser,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5001;
 
@@ -11,11 +12,16 @@ connectDB();
 
 const app = express();
 
+//DEV NOTE: Parse data coming from requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
-app.use('/api/products', productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

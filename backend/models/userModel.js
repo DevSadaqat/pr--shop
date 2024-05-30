6d1cca45e4b9522777f5a816a4cb83392bd69ae8
Sadaqat Ali <entrepreneurali@outlook.com>
2024-05-31 09:41:22 +1000

    feat: logout user and clear cookie

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,10 +26,20 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
-
+//DEV NOTE: Match user entered password to hashed password in database
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
+
+//DEV NOTE: Encrypt password using bcrypt
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 

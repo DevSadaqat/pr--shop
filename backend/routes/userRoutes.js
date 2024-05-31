9746f7ac5321d07d2f384698014a78889e2123d5
Sadaqat Ali <entrepreneurali@outlook.com>
2024-05-31 10:31:03 +1000

    feat: User profile endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,8 @@ import {
 } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddlware.js';
 
-router.route('/').post(registerUser).get(protect, admin, getUsers);
+router.route('/').get(protect, admin, getUsers);
+router.post('/register', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
 router

a9d944d3fa55489a10c6274e6741ac7bad171cd1
Sadaqat Ali <entrepreneurali@outlook.com>
2024-07-27 14:56:13 +1000

    feat: Product Image Uplaod

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,27 +1,13 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddlware.js';
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Resource not found');
-    }
-  })
-);
-
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').get(getProductById).put(protect, admin, updateProduct);
 export default router;
--- frontend/src/slices/productsSlice.js
+++ frontend/src/slices/productsSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from '../constants';
+import { PRODUCTS_URL, FILE_UPLOAD_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 //DEV NOTE: Builder will have methods to make queries from backend
@@ -8,6 +8,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      providesTags: ['Products'],
       keepUnusedDataFor: 5,
     }),
     getProductsById: builder.query({
@@ -16,8 +17,36 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         keepUnusedDataFor: 5,
       }),
     }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: 'POST',
+        //DEV NOTE: Invalidate tags to clear the cache of the products
+        invalidatesTags: ['Products'],
+      }),
+    }),
+    updateProduct: builder.mutation({
+      query: ({ data, productId }) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'PUT',
+        body: { ...data },
+        invalidatesTags: ['Products'],
+      }),
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: FILE_UPLOAD_URL,
+        method: 'POST',
+        body: data,
+      }),
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetProductsByIdQuery } =
-  productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductsByIdQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+  useUploadProductImageMutation,
+} = productsApiSlice;

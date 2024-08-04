778e1f0ac3e2b6af3e5423a75eff8320462e367a
Sadaqat Ali <entrepreneurali@outlook.com>
2024-08-04 12:53:37 +1000

    feat: Search box func

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,6 +5,7 @@ import Paginate from '../components/paginate';
 import { useGetProductsQuery } from '../slices/productsSlice';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { Link } from 'react-router-dom';
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
@@ -14,6 +15,11 @@ const HomeScreen = () => {
   });
   return (
     <>
+      {keyword && (
+        <Link to="/" className="btn btn-light">
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
